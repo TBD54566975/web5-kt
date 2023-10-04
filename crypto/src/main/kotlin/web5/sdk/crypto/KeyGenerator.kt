cc6d6c2d231cc21c5d6dea36d5a57819059555f5
Moe Jangda <moe@tbd.email>
2023-10-04 01:08:50 +0100

    add KtDoc

--- crypto/src/main/kotlin/web5/sdk/crypto/KeyGenerator.kt
+++ crypto/src/main/kotlin/web5/sdk/crypto/KeyGenerator.kt
@@ -7,17 +7,40 @@ import com.nimbusds.jose.jwk.KeyType
 public interface KeyGenOptions
 
 public interface KeyGenerator {
+  /**  Indicates the algorithm intended to be used with the key. */
   public val algorithm: Algorithm
+
+  /** Indicates the cryptographic algorithm family used with the key */
   public val keyType: KeyType
+
+  /** Generates a private key */
   public fun generatePrivateKey(options: KeyGenOptions? = null): JWK
 
+  /**
+   * Derives a public key from the private key provided. Applicable for asymmetric Key Generators only.
+   * Implementers of symmetric key generators should throw an UnsupportedOperation Exception
+   */
   public fun getPublicKey(privateKey: JWK): JWK
 
+  /**
+   * Converts a private key to bytes
+   */
   public fun privateKeyToBytes(privateKey: JWK): ByteArray
 
+  /**
+   * Converts a public key to bytes. Applicable for asymmetric Key Generators only.
+   * Implementers of symmetric key generators should throw an UnsupportedOperation Exception
+   */
   public fun publicKeyToBytes(publicKey: JWK): ByteArray
 
+  /**
+   * Converts a private key as bytes into a JWK
+   */
   public fun bytesToPrivateKey(privateKeyBytes: ByteArray): JWK
 
+  /**
+   * Converts a public key as bytes into a JWK. Applicable for asymmetric Key Generators only.
+   * Implementers of symmetric key generators should throw an UnsupportedOperation Exception
+   */
   public fun bytesToPublicKey(publicKeyBytes: ByteArray): JWK
 }
--- crypto/src/main/kotlin/web5/sdk/crypto/KeyManager.kt
+++ crypto/src/main/kotlin/web5/sdk/crypto/KeyManager.kt
@@ -5,13 +5,52 @@ import com.nimbusds.jose.Payload
 import com.nimbusds.jose.jwk.Curve
 import com.nimbusds.jose.jwk.JWK
 
+/**
+ * A key management interface that provides functionality for generating, storing, and utilizing
+ * private keys and their associated public keys. Implementations of this interface should handle
+ * the secure generation and storage of keys, providing mechanisms for utilizing them in cryptographic
+ * operations like signing.
+ *
+ * Example implementations might provide key management through various Key Management Systems (KMS),
+ * such as AWS KMS, Google Cloud KMS, Hardware Security Modules (HSM), or simple in-memory storage,
+ * each adhering to the same consistent API for usage within applications.
+ */
 public interface KeyManager {
+
   /**
-   * generates and stores a private key based on the arguments provided.
-   * @return an alias that can be used to reference the key
+   * Generates and securely stores a private key based on the provided algorithm and options,
+   * returning a unique alias that can be utilized to reference the generated key for future operations.
+   *
+   * @param algorithm The cryptographic algorithm to use for key generation.
+   * @param curve (Optional) The elliptic curve to use (relevant for EC algorithms).
+   * @param options (Optional) Additional options to control key generation behavior.
+   * @return A unique alias (String) that can be used to reference the stored key.
+   *
+   * Implementations should ensure secure storage of the generated keys, protecting against
+   * unauthorized access and ensuring cryptographic strength according to the provided parameters.
    */
   public fun generatePrivateKey(algorithm: Algorithm, curve: Curve? = null, options: KeyGenOptions? = null): String
+
+  /**
+   * Retrieves the public key associated with a previously stored private key, identified by the provided alias.
+   *
+   * @param keyAlias The alias referencing the stored private key.
+   * @return The associated public key in JWK (JSON Web Key) format.
+   *
+   * The function should provide the public key in a format suitable for external sharing and usage,
+   * enabling others to perform operations like verifying signatures or encrypting data for the private key holder.
+   */
   public fun getPublicKey(keyAlias: String): JWK
 
+  /**
+   * Signs the provided payload using the private key identified by the provided alias.
+   *
+   * @param keyAlias The alias referencing the stored private key.
+   * @param payload The data/payload to be signed.
+   *
+   * Implementations should ensure that the signing process is secured, utilizing secure cryptographic
+   * practices and safeguarding the private key during the operation. The specific signing algorithm
+   * used may depend on the type and parameters of the stored key.
+   */
   public fun sign(keyAlias: String, payload: Payload)
 }
