0878bd46494248d8a5e1d4ca89451ea5245da335
Tom Daffurn <tdaffurn@tbd.email>
2023-10-09 15:21:14 +1000

    Fix key type bug in Secp256k1

---
+++ crypto/src/test/kotlin/web5/sdk/crypto/Secp256k1Test.kt
@@ -0,0 +1,35 @@
+package web5.sdk.crypto
+
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.jwk.KeyType
+import com.nimbusds.jose.jwk.KeyUse
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Secp256k1Test {
+  @Test
+  fun `test key generation`() {
+    val privateKey = Secp256k1.generatePrivateKey()
+
+    Secp256k1.validateKey(privateKey)
+    assertEquals(JWSAlgorithm.ES256K, privateKey.algorithm)
+    assertEquals(KeyUse.SIGNATURE, privateKey.keyUse)
+    assertNotNull(privateKey.keyID)
+    assertEquals(KeyType.EC, privateKey.keyType)
+    assertTrue(privateKey.isPrivate)
+  }
+
+  @Test
+  fun `test public key`() {
+    val privateKey = Secp256k1.generatePrivateKey()
+
+    val publicKey = Secp256k1.computePublicKey(privateKey)
+
+    Secp256k1.validateKey(publicKey)
+    assertEquals(publicKey.keyID, privateKey.keyID)
+    assertEquals(JWSAlgorithm.ES256K, publicKey.algorithm)
+    assertEquals(KeyUse.SIGNATURE, publicKey.keyUse)
+    assertEquals(KeyType.EC, publicKey.keyType)
+    assertFalse(publicKey.isPrivate)
+  }
+}
