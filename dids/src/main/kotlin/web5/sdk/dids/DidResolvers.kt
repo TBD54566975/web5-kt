72250f2e6557005cf216ce818bef9d8cc4542800
Phoebe Lew <plew@squareup.com>
2023-10-10 20:53:51 +1100

    Add did:key resolver to DidResolvers and test

--- dids/src/main/kotlin/web5/sdk/dids/DidResolvers.kt
+++ dids/src/main/kotlin/web5/sdk/dids/DidResolvers.kt
@@ -5,8 +5,12 @@ import foundation.identity.did.DID
 /**
  * Type alias for a DID resolver function.
  * A DID resolver takes a DID URL as input and returns a [DidResolutionResult].
+ *
+ * @param didUrl The DID URL to resolve.
+ * @param options (Optional) Options for resolving the DID.
+ * @return A [DidResolutionResult] containing the resolved DID document or an error message.
  */
-public typealias DidResolver = (String) -> DidResolutionResult
+public typealias DidResolver = (String, ResolveDidOptions?) -> DidResolutionResult
 
 /**
  * Singleton object representing a collection of DID resolvers.
@@ -14,22 +18,25 @@ public typealias DidResolver = (String) -> DidResolutionResult
 public object DidResolvers {
 
   // A mutable map to store method-specific DID resolvers.
-  private val methodResolvers = mutableMapOf<String, DidResolver>()
+  private val methodResolvers = mutableMapOf<String, DidResolver>(
+    DidKey.methodName to DidKey.Companion::resolve
+  )
 
   /**
    * Resolves a DID URL using an appropriate resolver based on the DID method.
    *
    * @param didUrl The DID URL to resolve.
+   * @param options (Optional) Options for resolving the DID.
    * @return A [DidResolutionResult] containing the resolved DID document or an error message.
    * @throws IllegalArgumentException if resolving the specified DID method is not supported.
    */
-  public fun resolve(didUrl: String): DidResolutionResult {
+  public fun resolve(didUrl: String, options: ResolveDidOptions?): DidResolutionResult {
     val parsedDid = DID.fromString(didUrl)
     val resolver = methodResolvers.getOrElse(parsedDid.methodName) {
       throw IllegalArgumentException("Resolving did:${parsedDid.methodName} not supported")
     }
 
-    return resolver(didUrl)
+    return resolver(didUrl, options)
   }
 
   /**
