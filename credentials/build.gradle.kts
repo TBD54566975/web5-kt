8410886ad866254e6bb7cfe84285c00e904526e7
Moe Jangda <moe@tbd.email>
2023-10-09 22:19:34 -0500

    [credentials] remove unused dependencies

--- credentials/build.gradle.kts
+++ credentials/build.gradle.kts
@@ -13,20 +13,17 @@ repositories {
 }
 
 dependencies {
-  testImplementation(kotlin("test"))
-
   api("com.danubetech:verifiable-credentials-java:1.5.0")
-  api("com.nimbusds:nimbus-jose-jwt:9.34")
-  api("decentralized-identity:did-common-java:1.9.0")
-  api("decentralized-identity:uni-resolver-core:0.13.0")
 
   implementation(project(":dids"))
   implementation(project(":common"))
   implementation(project(":crypto"))
-  implementation("com.github.richardbergquist:java-multicodec:main-SNAPSHOT")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-  implementation("com.google.crypto.tink:tink:1.10.0")
   implementation("com.nfeld.jsonpathkt:jsonpathkt:2.0.1")
+  implementation("com.nimbusds:nimbus-jose-jwt:9.34")
+  implementation("decentralized-identity:did-common-java:1.9.0")
+
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
