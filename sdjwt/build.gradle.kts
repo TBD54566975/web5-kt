5fdad7c4266e443a6cae301c2bb9185574935275
Andres Uribe Gonzalez <auribe@squareup.com>
2023-10-25 04:45:28 -0400

    Prune gradle file.

---
+++ sdjwt/build.gradle.kts
@@ -0,0 +1,20 @@
+repositories {
+  mavenCentral()
+  maven {
+    url = uri("https://repo.danubetech.com/repository/maven-public")
+  }
+  maven("https://jitpack.io")
+}
+
+dependencies {
+  implementation("com.nimbusds:nimbus-jose-jwt:9.34")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+
+  testImplementation(project(":dids"))
+  testImplementation(project(":crypto"))
+  testImplementation("io.github.erdtman:java-json-canonicalization:1.1")
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
