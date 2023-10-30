6adf30a8b01e784c8a4ad0db5e8b8152fe216728
Moe Jangda <moe@tbd.email>
2023-10-30 15:46:12 -0700

    (crypto) add symmetric `import` `export` methods to `InMemoryKeyManager`

--- crypto/src/main/kotlin/web5/sdk/crypto/InMemoryKeyManager.kt
+++ crypto/src/main/kotlin/web5/sdk/crypto/InMemoryKeyManager.kt
@@ -86,6 +86,19 @@ public class InMemoryKeyManager : KeyManager {
   private fun getPrivateKey(keyAlias: String) =
     keyStore[keyAlias] ?: throw IllegalArgumentException("key with alias $keyAlias not found")
 
+  public fun import(keySet: List<Map<String, Any>>): List<String> {
+    val keyAliases = mutableListOf<String>()
+
+    for (jsonJwk in keySet) {
+      val jwk = JWK.parse(jsonJwk)
+      val keyAlias = import(jwk)
+
+      keyAliases.add(keyAlias)
+    }
+
+    return keyAliases
+  }
+
   /**
    * Imports [jwk] and returns the alias that refers to it.
    */
@@ -96,5 +109,15 @@ public class InMemoryKeyManager : KeyManager {
     }
     keyStore.putIfAbsent(kid, jwk)
     return kid
+  }
+
+  public fun export(): List<Map<String, Any>> {
+    val keySet = mutableListOf<Map<String, Any>>()
+    for (jwk in keyStore.values) {
+      val jsonJwk = jwk.toJSONObject()
+      keySet.add(jsonJwk)
+    }
+
+    return keySet
   }
 }
