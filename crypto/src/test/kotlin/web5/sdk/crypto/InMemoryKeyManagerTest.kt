02ef3a51dfca49d58197c870057efeb829bceab0
Moe Jangda <moe@tbd.email>
2023-10-30 16:10:20 -0700

    add test for `InMemoryKeyManager.export`

--- crypto/src/test/kotlin/web5/sdk/crypto/InMemoryKeyManagerTest.kt
+++ crypto/src/test/kotlin/web5/sdk/crypto/InMemoryKeyManagerTest.kt
@@ -3,8 +3,10 @@ package web5.sdk.crypto
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton
 import com.nimbusds.jose.jwk.Curve
+import com.nimbusds.jose.jwk.JWK
 import com.nimbusds.jose.jwk.gen.ECKeyGenerator
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import kotlin.test.assertEquals
 
 class InMemoryKeyManagerTest {
@@ -48,5 +50,18 @@ class InMemoryKeyManagerTest {
 
     val publicKey = keyManager.getPublicKey(alias)
     assertEquals(jwk.toPublicJWK(), publicKey)
+  }
+
+  @Test
+  fun `export returns all keys`() {
+    val keyManager = InMemoryKeyManager()
+    keyManager.generatePrivateKey(JWSAlgorithm.EdDSA, Curve.Ed25519)
+
+    val keySet = keyManager.export()
+    assertEquals(1, keySet.size)
+
+    assertDoesNotThrow {
+      JWK.parse(keySet[0])
+    }
   }
 }
