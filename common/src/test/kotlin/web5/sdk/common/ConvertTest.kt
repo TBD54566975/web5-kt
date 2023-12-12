21af04908df878b07716af208353ec8891a458be
Jiyoon Koo <jiyoon@squareup.com>
2023-12-12 13:57:19 -0700

    using TestCases to apply different input and output for each method being tested

--- common/src/test/kotlin/web5/sdk/common/ConvertTest.kt
+++ common/src/test/kotlin/web5/sdk/common/ConvertTest.kt
@@ -9,209 +9,159 @@ import kotlin.test.assertFailsWith
 class ConvertTest {
   @Nested
   inner class StringTest {
-    @Test
-    fun toBase64Url_NullKind() {
-      val input = "foo";
-      val expectedOutput = "Zm9v";
-
-      val output = Convert(input).toBase64Url()
-      assertEquals(output, expectedOutput)
-    }
+    inner class TestCase(
+      val input: Any,
+      val encodingFormat: EncodingFormat? = null,
+      val expectedOutput: Any? = null
+    )
 
     @Test
-    fun toBase64Url_Base64Kind() {
-      val input = "foo"
+    fun toBase64Url() {
 
-      val output = Convert(input, EncodingFormat.Base64Url).toBase64Url()
-      assertEquals(input, output)
-    }
+      val testCases = listOf(
+        TestCase("foo", null, "Zm9v"),
+        TestCase("foo", EncodingFormat.Base64Url, "foo")
+      )
 
-    @Test
-    fun toBase64Url_UnsupportedEncodingFormat() {
-      val input = "foo"
-
-      assertFailsWith<UnsupportedOperationException> {
-        Convert(input, EncodingFormat.Base58Btc).toBase64Url()
+      for (testCase in testCases) {
+        val output = Convert(testCase.input, testCase.encodingFormat).toBase64Url()
+        assertEquals(testCase.expectedOutput, output)
       }
     }
 
     @Test
-    fun toBase64Url_UnsupportedValueType() {
-      val input = 1
+    fun toBase64Url_FailCases() {
 
-      assertFailsWith<UnsupportedOperationException> {
-        Convert(input, EncodingFormat.Base58Btc).toBase64Url()
+      val testCases = listOf(
+        TestCase("foo", EncodingFormat.Base58Btc),
+        TestCase(1)
+      )
+
+      for (testCase in testCases) {
+        assertFailsWith<UnsupportedOperationException> {
+          Convert(testCase.input, testCase.encodingFormat).toBase64Url()
+        }
       }
     }
 
     @Test
-    fun toBase58Btc_Base58Kind() {
-      val input = "foo"
+    fun toBase58Btc() {
 
-      val output = Convert(input, EncodingFormat.Base58Btc).toBase58Btc()
-      assertEquals(input, output)
-    }
+      val testCases = listOf(
+        TestCase("foo", EncodingFormat.Base58Btc, "foo"),
+        TestCase("foo", EncodingFormat.Base64Url, "AdX"),
+        TestCase("bar", EncodingFormat.ZBase32, "F"),
+        TestCase("foo", null, "bQbp")
+        )
 
-    @Test
-    fun toBase58Btc_Base64Kind() {
-      val input = "foo"
-      val expectedOutput = "AdX"
-      val output = Convert(input, EncodingFormat.Base64Url).toBase58Btc()
-      assertEquals(expectedOutput, output)
-    }
-
-    @Test
-    fun toBase58Btc_ZBase32Kind() {
-      val input = "bar"
-      val expectedOutput = "F"
-      val output = Convert(input, EncodingFormat.ZBase32).toBase58Btc()
-      assertEquals(expectedOutput, output)
-    }
-
-    @Test
-    fun toBase58Btc_NullKind() {
-      val input = "foo"
-      val expectedOutput = "bQbp"
-      val output = Convert(input).toBase58Btc()
-      assertEquals(expectedOutput, output)
-    }
-
-    @Test
-    fun toBase58Btc_UnsupportedValueType() {
-      val input = 1
-
-      assertFailsWith<UnsupportedOperationException> {
-        Convert(input).toBase58Btc()
+      for (testCase in testCases) {
+        val output = Convert(testCase.input, testCase.encodingFormat).toBase58Btc()
+        assertEquals(testCase.expectedOutput, output)
       }
     }
 
     @Test
-    fun toZBase32_Base58Kind() {
-      val input = "foo"
-      val expectedOutput = "y869r"
+    fun toBase58Btc_FailCases() {
 
-      val output = Convert(input, EncodingFormat.Base58Btc).toZBase32()
-      assertEquals(expectedOutput, output)
-    }
+      val testCases = listOf(
+        TestCase(1)
+      )
 
-    @Test
-    fun toZBase32_Base64Kind() {
-      val input = "foo"
-      val expectedOutput = "x4fy"
-
-      val output = Convert(input, EncodingFormat.Base64Url).toZBase32()
-      assertEquals(expectedOutput, output)
-    }
-
-    @Test
-    fun toZBase32_ZBase32Kind() {
-      val input = "foo"
-      val output = Convert(input, EncodingFormat.ZBase32).toZBase32()
-      assertEquals(input, output)
-    }
-
-    @Test
-    fun toZBase32_NullKind() {
-      val input = "foo"
-      val expectedOutput = "c3zs6"
-      val output = Convert(input).toZBase32()
-      assertEquals(expectedOutput, output)
-    }
-
-    @Test
-    fun toZBase32_UnsupportedValueType() {
-      val input = 1
-
-      assertFailsWith<UnsupportedOperationException> {
-        Convert(input).toZBase32()
+      for (testCase in testCases) {
+        assertFailsWith<UnsupportedOperationException> {
+          Convert(testCase.input, testCase.encodingFormat).toBase58Btc()
+        }
       }
     }
 
     @Test
-    fun toStr_Base64Kind() {
-      val input = "Zm9v"
-      val expectedOutput = "foo"
-      val output = Convert(input, EncodingFormat.Base64Url).toStr()
+    fun toZBase32() {
 
-      assertEquals(expectedOutput, output)
-    }
+      val testCases = listOf(
+        TestCase("foo", EncodingFormat.Base58Btc, "y869r"),
+        TestCase("foo", EncodingFormat.Base64Url, "x4fy"),
+        TestCase("foo", EncodingFormat.ZBase32, "foo"),
+        TestCase("foo", null, "c3zs6")
+        )
 
-    @Test
-    fun toStr_ZBase32Kind() {
-      val input = "c3zs6"
-      val expectedOutput = "foo"
-      val output = Convert(input, EncodingFormat.ZBase32).toStr()
-
-      assertEquals(expectedOutput, output)
-    }
-
-    @Test
-    fun toStr_NullKind() {
-      val input = "foo"
-      val output = Convert(input).toStr()
-
-      assertEquals(input, output)
-    }
-
-    @Test
-    fun toStr_UnsupportedKind() {
-      val input = "foo"
-
-      assertFailsWith<UnsupportedOperationException> {
-        Convert(input, EncodingFormat.Base58Btc).toStr()
+      for (testCase in testCases) {
+        val output = Convert(testCase.input, testCase.encodingFormat).toZBase32()
+        assertEquals(testCase.expectedOutput, output)
       }
     }
 
     @Test
-    fun toStr_UnsupportedValueType() {
-      val input = 1
+    fun toZBase32_FailCases() {
 
-      assertFailsWith<UnsupportedOperationException> {
-        Convert(input).toStr()
+      val testCases = listOf(
+        TestCase(1)
+      )
+
+      for (testCase in testCases) {
+        assertFailsWith<UnsupportedOperationException> {
+          Convert(testCase.input, testCase.encodingFormat).toZBase32()
+        }
       }
     }
 
     @Test
-    fun toByteArray_Base58Kind() {
-      val input = "bQbp"
-      val expectedOutput = "foo".toByteArray()
-      val output = Convert(input, EncodingFormat.Base58Btc).toByteArray()
+    fun toStr() {
 
-      assertArrayEquals(expectedOutput, output)
+      val testCases = listOf(
+        TestCase("Zm9v", EncodingFormat.Base64Url, "foo"),
+        TestCase("c3zs6", EncodingFormat.ZBase32, "foo"),
+        TestCase("foo", null, "foo")
+        )
+
+      for (testCase in testCases) {
+        val output = Convert(testCase.input, testCase.encodingFormat).toStr()
+        assertEquals(testCase.expectedOutput, output)
+      }
     }
 
     @Test
-    fun toByteArray_Base64Kind() {
-      val input = "Zm9v"
-      val expectedOutput = "foo".toByteArray()
-      val output = Convert(input, EncodingFormat.Base64Url).toByteArray()
+    fun toStr_FailCases() {
 
-      assertArrayEquals(expectedOutput, output)
+      val testCases = listOf(
+        TestCase("foo", EncodingFormat.Base58Btc),
+        TestCase(1)
+      )
+
+      for (testCase in testCases) {
+        assertFailsWith<UnsupportedOperationException> {
+          Convert(testCase.input, testCase.encodingFormat).toStr()
+        }
+      }
     }
 
     @Test
-    fun toByteArray_ZBase32Kind() {
-      val input = "c3zs6"
-      val expectedOutput = "foo".toByteArray()
-      val output = Convert(input, EncodingFormat.ZBase32).toByteArray()
+    fun toByteArray() {
 
-      assertArrayEquals(expectedOutput, output)
+      val testCases = listOf(
+        TestCase("bQbp", EncodingFormat.Base58Btc, "foo".toByteArray()),
+        TestCase("Zm9v", EncodingFormat.Base64Url, "foo".toByteArray()),
+        TestCase("c3zs6", EncodingFormat.ZBase32, "foo".toByteArray()),
+        TestCase("foo".toByteArray(), null, "foo".toByteArray())
+      )
+
+      for (testCase in testCases) {
+        val output = Convert(testCase.input, testCase.encodingFormat).toByteArray()
+        assertArrayEquals(testCase.expectedOutput as ByteArray, output)
+      }
     }
 
     @Test
-    fun toByteArray_NullKind() {
-      val input = "foo".toByteArray()
-      val output = Convert(input).toByteArray()
+    fun toByteArray_FailCases() {
 
-      assertArrayEquals(input, output)
+      val testCases = listOf(
+        TestCase(1)
+      )
+
+      for (testCase in testCases) {
+        assertFailsWith<UnsupportedOperationException> {
+          Convert(testCase.input, testCase.encodingFormat).toByteArray()
+        }
+      }
     }
-
-    @Test
-    fun toByteArray_UnsupportedValueKind() {
-      val input = 1
-
-      assertFailsWith<UnsupportedOperationException> {
-        Convert(input).toByteArray()
-      }    }
   }
 }
