4a30bd8f98e029848a8a53e8878b7ad1308360be
Kirah Sapong <kirah@tbd.email>
2024-02-02 11:09:06 -0800

    Add ed25519 verify and sign tests against vectors

---
+++ crypto/src/test/kotlin/web5/sdk/crypto/Ed25519Test.kt
@@ -0,0 +1,107 @@
+package web5.sdk.crypto
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.nimbusds.jose.jwk.OctetKeyPair
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import web5.sdk.testing.TestVectors
+import java.io.File
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+class Web5TestVectorsCryptoEd25519 {
+  data class VerifyTestInput(
+    val key: Map<String, Any>,
+    val signature: String,
+    val data: String
+  )
+
+  data class SignTestInput(
+    val key: Map<String, Any>,
+    val data: String
+  )
+
+  private val mapper = jacksonObjectMapper()
+
+  @Test
+  fun sign() {
+    val typeRef = object : TypeReference<TestVectors<SignTestInput, String>>() {}
+    val testVectors = mapper.readValue(File("../web5-spec/test-vectors/crypto_ed25519/sign.json"), typeRef)
+
+    testVectors.vectors.filter { it.errors == false }.forEach { vector ->
+      // Convert input data from hex to byte array
+      val inputByteArray: ByteArray = hexStringToByteArray(vector.input.data)
+      val jwkMap = vector.input.key
+
+      val ed25519Jwk = OctetKeyPair.parse(jwkMap.toString())
+
+      val signedByteArray: ByteArray = Ed25519.sign(ed25519Jwk, inputByteArray)
+
+      // Convert signed byte array to hex string for comparison
+      val signedHex = byteArrayToHexString(signedByteArray)
+
+      assertEquals(vector.output, signedHex)
+    }
+
+    testVectors.vectors.filter { it.errors == true }.forEach { vector ->
+      assertFails {
+        // Convert input data from hex to byte array
+        val inputByteArray: ByteArray = hexStringToByteArray(vector.input.data)
+        val jwkMap = vector.input.key
+
+        val ed25519Jwk = OctetKeyPair.parse(jwkMap.toString())
+
+        Ed25519.sign(ed25519Jwk, inputByteArray)
+      }
+    }
+  }
+
+  @Test
+  fun verify() {
+    val typeRef = object : TypeReference<TestVectors<VerifyTestInput, Boolean>>() {}
+    val testVectors = mapper.readValue(File("../web5-spec/test-vectors/crypto_ed25519/verify.json"), typeRef)
+
+    testVectors.vectors.filter { it.errors == false }.forEach { vector ->
+      println("Testing vector for ${vector.description}")
+      val key = OctetKeyPair.parse(vector.input.key)
+      val data = hexStringToByteArray(vector.input.data)
+      val signature = hexStringToByteArray(vector.input.signature)
+      if (vector.output == true) {
+        assertDoesNotThrow {
+          Ed25519.verify(key, data, signature)
+        }
+      } else {
+        assertFails {
+          Ed25519.verify(key, data, signature)
+        }
+      }
+    }
+
+    testVectors.vectors.filter { it.errors == true }.forEach { vector ->
+      assertFails {
+        println("Testing vector for ${vector.description}")
+        val key = OctetKeyPair.parse(vector.input.key)
+        val data = hexStringToByteArray(vector.input.data)
+        val signature = hexStringToByteArray(vector.input.signature)
+        Ed25519.verify(key, data, signature)
+      }
+    }
+
+  }
+
+  // Utility function to convert hex string to byte array
+  private fun hexStringToByteArray(s: String): ByteArray {
+    val len = s.length
+    val data = ByteArray(len / 2)
+    for (i in 0 until len step 2) {
+      data[i / 2] = ((Character.digit(s[i], 16) shl 4) + Character.digit(s[i + 1], 16)).toByte()
+    }
+    return data
+  }
+
+  // Utility function to convert byte array to hex string
+  private fun byteArrayToHexString(bytes: ByteArray): String {
+    return bytes.joinToString("") { "%02x".format(it) }
+  }
+}
